aaa38b66022a488900ba6ecb494bda981ee110b6
Tony <tonyschneider112@hotmail.com>
2020-07-02 20:00:08 +0300

    project commit

--- app/src/main/java/com/example/time2eat/MenuActivity.java
+++ app/src/main/java/com/example/time2eat/MenuActivity.java
@@ -1,9 +1,7 @@
 package com.example.time2eat;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
-//import android.support.v7.widget.Toolbar;
-
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -14,15 +12,14 @@ public class MenuActivity extends AppCompatActivity {
     public void importProducts(){
     }
 
-    private Toolbar toolbar;
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        //toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
 
     }
 
